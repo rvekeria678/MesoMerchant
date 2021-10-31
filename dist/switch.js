9de683f3879eb850ab86e8a8ff3518fa6a8c561d
rvekeria999 <81255193+rvekeria999@users.noreply.github.com>
2021-10-31 15:45:56 -0400

    Additonal Button Functionality

--- dist/switch.js
+++ dist/switch.js
@@ -1,32 +1,56 @@
 $(document).ready(function(){
+    $("#s1").show();
+    $("#s2").hide();
+    $("#s3").hide();
+    $("#s4").hide();
+    $("#s5").hide();
+    $("#s6").hide();
     $("#btn1").click(function(){
-        $("header").show();
-        $("#homeimg").show();
-        $("p").show();
+        $("#s1").show();
+        $("#s2").hide();
+        $("#s3").hide();
+        $("#s4").hide();
+        $("#s5").hide();
+        $("#s6").hide();
     });
     $("#btn2").click(function() {
-        $("header").hide();
-        $("#homeimg").hide();
-        $("p").hide();
+        $("#s1").hide();
+        $("#s2").show();
+        $("#s3").hide();
+        $("#s4").hide();
+        $("#s5").hide();
+        $("#s6").hide();
     });
     $("#btn3").click(function() {
-        $("header").hide();
-        $("#homeimg").hide();
-        $("p").hide();
+        $("#s1").hide();
+        $("#s2").hide();
+        $("#s3").show();
+        $("#s4").hide();
+        $("#s5").hide();
+        $("#s6").hide();
     });
     $("#btn4").click(function() {
-        $("header").hide();
-        $("#homeimg").hide();
-        $("p").hide();
+        $("#s1").hide();
+        $("#s2").hide();
+        $("#s3").hide();
+        $("#s4").show();
+        $("#s5").hide();
+        $("#s6").hide();
     });
     $("#btn5").click(function() {
-        $("header").hide();
-        $("#homeimg").hide();
-        $("p").hide();
+        $("#s1").hide();
+        $("#s2").hide();
+        $("#s3").hide();
+        $("#s4").hide();
+        $("#s5").show();
+        $("#s6").hide();
     });
     $("#btn6").click(function() {
-        $("header").hide();
-        $("#homeimg").hide();
-        $("p").hide();
+        $("#s1").hide();
+        $("#s2").hide();
+        $("#s3").hide();
+        $("#s4").hide();
+        $("#s5").hide();
+        $("#s6").show();
     });
 });
