f07e2c984c81eea092d9ae7c690862f042be46ca
rvekeria999 <81255193+rvekeria999@users.noreply.github.com>
2021-10-31 14:30:47 -0400

    Added Button Actions

---
+++ dist/switch.js
@@ -0,0 +1,18 @@
+function home() {
+    document.getElementById('content').innerHTML = "<header>Home</header>";
+}
+function team() {
+    document.getElementById('content').innerHTML = "<header>Meet the Team</header>";
+}
+function research() {
+    document.getElementById('content').innerHTML = "<header>Research</header>";
+}
+function assignments() {
+    document.getElementById('content').innerHTML = "<header>Intermediate Assignments</header>";
+}
+function agenda() {
+    document.getElementById('content').innerHTML = "<header>Agenda</header>";
+}
+function proposal() {
+    document.getElementById('content').innerHTML = "<header>Project Proposal</header>";
+}
